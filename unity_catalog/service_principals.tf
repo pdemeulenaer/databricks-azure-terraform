37107d198dabd380adbb54562792b18b93c30791
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-11 17:45:38 +0200

    restore service account

--- unity_catalog/service_principals.tf
+++ unity_catalog/service_principals.tf
@@ -1,14 +1,14 @@
-# // initialize provider at Azure account-level
-# provider "databricks" {
-#   alias      = "azure_account"
-#   host       = "https://accounts.azuredatabricks.net"
-#   account_id = var.databricks_account_id
-#   auth_type  = "azure-cli"
-# }
+// initialize provider at Azure account-level
+provider "databricks" {
+  alias      = "azure_account"
+  host       = "https://accounts.azuredatabricks.net"
+  account_id = var.databricks_account_id
+  auth_type  = "azure-cli"
+}
 
-# resource "databricks_service_principal" "sp" {
-#   provider       = databricks.azure_account
-#   application_id = var.application_id 
-#   display_name = "Data Engineers"
-#   workspace_access = true
-# }
+resource "databricks_service_principal" "sp" {
+  provider       = databricks.azure_account
+  application_id = var.application_id 
+  display_name = "Data Engineers"
+  workspace_access = true
+}
