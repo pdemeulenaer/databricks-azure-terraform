079c977b548a33e4739aca42e899fee976c165a8
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-11 17:56:54 +0200

    make sp admin

--- unity_catalog/service_principals.tf
+++ unity_catalog/service_principals.tf
@@ -10,5 +10,10 @@ resource "databricks_service_principal" "sp" {
   provider       = databricks.azure_account
   application_id = var.application_id 
   display_name = "Data Engineers"
-  workspace_access = true
+  workspace_access = true # meaningful or not?
+}
+
+resource "databricks_permission_assignment" "add_admin_spn" {
+  principal_id = databricks_service_principal.sp.id
+  permissions  = ["ADMIN"]
 }
