aa50f7a713e83b51b215f77e01f5ae62ed9c3543
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-12 18:06:40 +0200

    change pool config

--- workspace_configuration/configurations.tf
+++ workspace_configuration/configurations.tf
@@ -2,7 +2,7 @@
 resource "databricks_instance_pool" "smallest_nodes" {
   instance_pool_name = "Smallest Nodes (${data.databricks_current_user.me.alphanumeric})"
   min_idle_instances = 0
-  max_capacity       = 30
+  max_capacity       = 10
   node_type_id       = data.databricks_node_type.smallest.id
   preloaded_spark_versions = [
     data.databricks_spark_version.latest.id
