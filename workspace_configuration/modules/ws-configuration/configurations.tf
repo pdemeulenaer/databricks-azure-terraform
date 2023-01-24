1e575c741e1c9cbbb13d88133c6ec21a7ca6f3ac
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-24 15:20:30 +0200

    disable staging config

--- workspace_configuration/modules/ws-configuration/configurations.tf
+++ workspace_configuration/modules/ws-configuration/configurations.tf
@@ -57,47 +57,47 @@ resource "databricks_repo" "nutter_in_home" {
 }
 
 
-# ================
-# STAGING PLATFORM
-# ================
+# # ================
+# # STAGING PLATFORM
+# # ================
 
-# POOL CREATION
-resource "databricks_instance_pool" "pool" {
-  provider = databricks.staging
-  instance_pool_name = "Smallest Nodes (${var.current_user_alphanumeric})" # "Smallest Nodes (${data.databricks_current_user.me.alphanumeric})"
-  min_idle_instances = 0
-  max_capacity       = 10
-  node_type_id       = var.node_type_id # data.databricks_node_type.smallest.id
-  preloaded_spark_versions = [
-    var.spark_version_id # data.databricks_spark_version.latest.id
-  ]
+# # POOL CREATION
+# resource "databricks_instance_pool" "pool" {
+#   provider = databricks.staging
+#   instance_pool_name = "Smallest Nodes (${var.current_user_alphanumeric})" # "Smallest Nodes (${data.databricks_current_user.me.alphanumeric})"
+#   min_idle_instances = 0
+#   max_capacity       = 10
+#   node_type_id       = var.node_type_id # data.databricks_node_type.smallest.id
+#   preloaded_spark_versions = [
+#     var.spark_version_id # data.databricks_spark_version.latest.id
+#   ]
 
-  idle_instance_autotermination_minutes = 20
-}
+#   idle_instance_autotermination_minutes = 20
+# }
 
-# GLOBAL INIT SCRIPTS
-resource "databricks_global_init_script" "init1" {
-  provider = databricks.staging  
-  source = "${path.module}/../../global_init_scripts/init.sh"
-  name   = "test global init script"
-  enabled = true # by default false
-  position = 0 # position of Global init script (0=first) if multiple
-}
+# # GLOBAL INIT SCRIPTS
+# resource "databricks_global_init_script" "init1" {
+#   provider = databricks.staging  
+#   source = "${path.module}/../../global_init_scripts/init.sh"
+#   name   = "test global init script"
+#   enabled = true # by default false
+#   position = 0 # position of Global init script (0=first) if multiple
+# }
 
-# CLUSTER CREATIONS
+# # CLUSTER CREATIONS
 
-# GITHUB INTEGRATION
-# https://registry.terraform.io/providers/databricks/databricks/latest/docs/resources/git_credential
+# # GITHUB INTEGRATION
+# # https://registry.terraform.io/providers/databricks/databricks/latest/docs/resources/git_credential
 
-resource "databricks_git_credential" "github" {
-  provider = databricks.staging
-  git_username          = "pdemeulenaer"
-  git_provider          = "gitHub"
-  personal_access_token = var.github_token
-}
+# resource "databricks_git_credential" "github" {
+#   provider = databricks.staging
+#   git_username          = "pdemeulenaer"
+#   git_provider          = "gitHub"
+#   personal_access_token = var.github_token
+# }
 
-# Cloning a repo
-resource "databricks_repo" "nutter_in_home" {
-  provider = databricks.staging
-  url = var.git_repo
-}
+# # Cloning a repo
+# resource "databricks_repo" "nutter_in_home" {
+#   provider = databricks.staging
+#   url = var.git_repo
+# }
