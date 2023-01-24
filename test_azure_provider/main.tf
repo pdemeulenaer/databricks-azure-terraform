b1f7f6205e68e4534a89ce13e275bdb3f642b8fd
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-24 12:17:59 +0200

    add test

---
+++ test_azure_provider/main.tf
@@ -0,0 +1,50 @@
+terraform {
+ 
+    required_providers {
+        # azurerm = "2.33" #"~> 2.33"
+		azurerm = {
+			source = "hashicorp/azurerm"
+			version = "3.40.0"
+		}		
+        random  = "~> 2.2"
+    }
+	
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "random_string" "naming" {
+  special = false
+  upper   = false
+  length  = 6
+}
+
+data "azurerm_client_config" "current" {
+}
+
+data "external" "me" {
+  program = ["az", "account", "show", "--query", "user"]
+}
+
+locals {
+  # prefix = "databricks-tf-${random_string.naming.result}"
+  prefix = "testing"
+  tags = {
+    # Environment = "Demo"
+    Owner       = lookup(data.external.me.result, "name")
+    Source      = "terraform"
+  }
+}
+
+# 1. DEV WORKSPACE CREATION
+# =========================
+
+resource "azurerm_resource_group" "test" {
+  name     = "${local.prefix}-rg"
+  location = "westeurope"
+  tags = merge(local.tags, {
+    environment = "none"
+  })
+}
