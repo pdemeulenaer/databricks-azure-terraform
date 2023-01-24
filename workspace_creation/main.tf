4309587ec73242dea2ab12fa012a349e7e4ee051
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2023-01-24 12:02:30 +0200

    comment creation output

--- workspace_creation/main.tf
+++ workspace_creation/main.tf
@@ -78,10 +78,10 @@ output "databricks_host_staging" {
   value = "https://${azurerm_databricks_workspace.staging.workspace_url}/"
 }
 
-resource "local_file" "staging_url" {
-    content  = "https://${azurerm_databricks_workspace.staging.workspace_url}/"
-    filename = "staging_ws.txt"
-}
+# resource "local_file" "staging_url" {
+#     content  = "https://${azurerm_databricks_workspace.staging.workspace_url}/"
+#     filename = "staging_ws.txt"
+# }
 
 
 # 3. PROD WORKSPACE CREATION
